6881565e0270dc86d920f72e8f9d43ddfabdb1a3
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-09-17 05:40:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,7 @@ def monopoly_with_fourth_tier
       :four_pieces_owned => 200}, 
     :names => {
       :reading_railroad => {"mortgage_value" => "$100"},
-      :b_and_o_railroad => {},
+      :b_and_o_railroad => {"mortgage_value"=>"$200"},
       :pennsylvania_railroad => {},
       :shortline_railroad => {}
     }}}
